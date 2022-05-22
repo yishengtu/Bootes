fa90b9d6350020f4362046bf0b052743df4e05fc
yishengtu <yt2cr@virginia.edu>
2022-05-22 10:22:13 -0400

    float to double

--- src/algorithm/timeintegration.hpp
+++ src/algorithm/timeintegration.hpp
@@ -184,32 +184,6 @@ void first_order(mesh &m, double &dt){
     // step 4: apply boundary conditions
     apply_boundary_condition(m);
 
-
-    for (int kk = m.x3s; kk < m.x3l; kk++){
-        for (int jj = m.x2s; jj < m.x2l; jj++){
-            for (int ii = m.x1s; ii < m.x1l; ii++){
-                // 1
-                // if (abs(m.x1v(ii) * cos(m.x2v(jj))) < 0.3){
-                if (abs(m.x2v(jj) - M_PI / 2.) < M_PI / 20. && abs(m.x1v(ii) - 1.0) < 0.1){
-                    m.cons(IDN, kk, jj, ii) = 2.0;
-                    m.cons(IM1, kk, jj, ii) = 5. * m.cons(IDN, kk, jj, ii);
-                    m.cons(IM2, kk, jj, ii) = 0.0; //- 0.5 * cos(m.x2v(jj)) * m.cons(IDN, kk, jj, ii);
-                    m.cons(IM1, kk, jj, ii) += 0.01 * ((double) rand()/RAND_MAX  - 0.5) * m.cons(IDN, kk, jj, ii);
-                    m.cons(IM2, kk, jj, ii) += 0.01 * ((double) rand()/RAND_MAX  - 0.5) * m.cons(IDN, kk, jj, ii);
-                    m.cons(IM3, kk, jj, ii) = 0;
-                    m.prim(IPN, kk, jj, ii) = 2.5;
-                    double vel1 = m.cons(IM1, kk, jj, ii) / m.cons(IDN, kk, jj, ii);
-                    double vel2 = m.cons(IM2, kk, jj, ii) / m.cons(IDN, kk, jj, ii);
-                    double vel3 = m.cons(IM3, kk, jj, ii) / m.cons(IDN, kk, jj, ii);
-                    m.cons(IEN, kk, jj, ii) = ene(m.cons(IDN, kk, jj, ii), m.prim(IPN, kk, jj, ii),
-                                                                  vel1,
-                                                                  vel2,
-                                                                  vel3, m.hydro_gamma);
-                }
-            }
-        }
-    }
-
 }
 
 #endif // TIME_INTEGRATION_HPP_
