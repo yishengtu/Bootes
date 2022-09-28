a2d7c27c2cbe4a7bd0b809ce3b0b95698469c8dd
hsuchunyen77 <hsuchunyen77@gmail.com>
2022-09-28 19:59:42 +0000

    ORG	add a comment in util.cpp

--- src/algorithm/util/util.cpp
+++ src/algorithm/util/util.cpp
@@ -52,7 +52,8 @@ void cross_prod(double a[3], double b[3], double a_cross_b[3]){
 
 
 int searchsorted(double x, BootesArray<double> &xlist){
-    /*for (int ii = 0; ii < xlist.shape()[0]; ii++){
+    // Need to be updated
+	/*for (int ii = 0; ii < xlist.shape()[0]; ii++){
         cout << xlist(ii) << '\t';
     }
     cout << endl;*/
